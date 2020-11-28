4a09ecfb17cb476bc3972b80be024141bedfa591
nightlovell314 <ilyazenkinn@mail.ru>
2020-11-28 20:06:52 +0300

    подкорректировал EventFragment

--- app/src/main/java/com/startup/eventsearcher/main/ui/events/EventFragment.java
+++ app/src/main/java/com/startup/eventsearcher/main/ui/events/EventFragment.java
@@ -17,49 +17,20 @@ import com.startup.eventsearcher.main.ui.events.dummy.DummyContent;
 
 public class EventFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
     public EventFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static EventFragment newInstance(int columnCount) {
-        EventFragment fragment = new EventFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_event_list, container, false);
-
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyEventRecyclerViewAdapter(DummyContent.ITEMS));
-        }
+        RecyclerView recyclerView = view.findViewById(R.id.eventList);
+        recyclerView.setAdapter(new MyEventRecyclerViewAdapter(DummyContent.ITEMS));
         return view;
     }
 }
