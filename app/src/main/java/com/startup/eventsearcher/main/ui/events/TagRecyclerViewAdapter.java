d95fe77a9e301ad2639f0a5ee61846016cff6529
nightlovell314 <ilyazenkinn@mail.ru>
2020-12-04 00:42:34 +0300

    add animation of tag

--- app/src/main/java/com/startup/eventsearcher/main/ui/events/TagRecyclerViewAdapter.java
+++ app/src/main/java/com/startup/eventsearcher/main/ui/events/TagRecyclerViewAdapter.java
@@ -1,9 +1,14 @@
 package com.startup.eventsearcher.main.ui.events;
 
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,8 +38,27 @@ public class TagRecyclerViewAdapter extends RecyclerView.Adapter<TagRecyclerView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         holder.tagText.setText(listTags.get(position));
+
+        holder.tagCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int startColor = ContextCompat.getColor(view.getContext(), R.color.primaryLightColor);
+                int endColor = ContextCompat.getColor(view.getContext(), R.color.primaryDarkColor);
+                int duration = 300;
+                if (holder.tagCard.getCardBackgroundColor().getDefaultColor() == startColor) {
+                    showImageSearchOnTheTag(holder.imageView, true);
+                    holder.tagText.setTextColor(ContextCompat.getColor(view.getContext(), R.color.white));
+                    startColorAnimation(startColor, endColor, duration, holder.tagCard);
+                } else {
+                    showImageSearchOnTheTag(holder.imageView, false);
+                    holder.tagText.setTextColor(ContextCompat.getColor(view.getContext(), R.color.black));
+                    startColorAnimation(endColor, startColor, duration, holder.tagCard);
+                }
+            }
+        });
     }
 
     @Override
@@ -42,26 +66,39 @@ public class TagRecyclerViewAdapter extends RecyclerView.Adapter<TagRecyclerView
         return listTags.size();
     }
 
+    private void startColorAnimation(int startColor, int endColor, int duration, final CardView tagCard) {
+        ValueAnimator anim;
+        anim = ValueAnimator.ofArgb(startColor, endColor)
+                .setDuration(duration);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                if (tagCard != null) {
+                    tagCard.setCardBackgroundColor((Integer) valueAnimator.getAnimatedValue());
+                }
+            }
+        });
+        anim.start();
+    }
+
+    private void showImageSearchOnTheTag(ImageView imageView, boolean show){
+        if (show){
+            imageView.setImageResource(R.drawable.ic_search);
+        }else {
+            imageView.setImageDrawable(null);
+        }
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder{
         public final TextView tagText;
         public final CardView tagCard;
+        public final ImageView imageView;
 
         public ViewHolder(View view) {
             super(view);
             tagText = view.findViewById(R.id.tagCardText);
             tagCard = view.findViewById(R.id.tagCard);
-
-            tagCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (tagCard.getCardBackgroundColor().getDefaultColor() == ContextCompat.getColor(view.getContext(), R.color.primaryLightColor)){
-                        tagCard.setCardBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.primaryDarkColor));
-                    }else {
-                        tagCard.setCardBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.primaryLightColor));
-                    }
-                    Toast.makeText(view.getContext(), "press tag", Toast.LENGTH_SHORT).show();
-                }
-            });
+            imageView = view.findViewById(R.id.tagImageSearch);
         }
     }
 }
